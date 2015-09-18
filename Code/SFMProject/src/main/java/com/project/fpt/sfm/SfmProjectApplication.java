1b338dd25401f0aa9ad67c0010b5aecc4713db39
khacvy <khacvy93@gmail.com>
2015-09-18 11:26:37 +0700

    Add Report2 final

---
+++ Code/SFMProject/src/main/java/com/project/fpt/sfm/SfmProjectApplication.java
@@ -0,0 +1,12 @@
+package com.project.fpt.sfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfmProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfmProjectApplication.class, args);
+    }
+}
