9d4458cade264411deb40f9a54723619345b4f0a
khacvy <khacvy93@gmail.com>
2015-09-21 00:19:29 +0700

    Commit meeting minuties. Update some code

--- Code/SFMProject/src/main/java/com/project/fpt/sfm/web/controller/HomeController.java
+++ Code/SFMProject/src/main/java/com/project/fpt/sfm/web/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.project.fpt.sfm.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,7 +11,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping("/home")
-    public String homePage(){
+    public String homePage(Model model){
+        model.addAttribute("sidebar", "student/student-sidebar");
+
+
+        return "home";
+    }
+
+    @RequestMapping("/student")
+    public String studenPage(Model model){
+        model.addAttribute("sidebar", "student/student-sidebar");
+
+        return "home";
+    }
+
+    @RequestMapping("/student/hocphidukien")
+    public String studentHocPhiDuKien(Model model){
+        model.addAttribute("content", "student/hocphidukien");
+        model.addAttribute("sidebar", "student/student-sidebar");
+
+        return "home";
+    }
+
+    @RequestMapping("/student/cackydahoc")
+    public String studentCackydahoc(Model model){
+        model.addAttribute("content", "student/cackydahoc");
+        model.addAttribute("sidebar", "student/student-sidebar");
+
+        return "home";
+    }
+
+    @RequestMapping("/student/cacmonchuahoanthanh")
+    public String studentCacMonChuaHoanThanh(Model model){
+        model.addAttribute("content", "student/cacmonchuahoanthanh");
+        model.addAttribute("sidebar", "student/student-sidebar");
+
         return "home";
     }
 }
